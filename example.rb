4e53c4cb59ee0aecfc78e70f80482a9360a4d4d5
nudelx <alex.nudelman@samanage.com>
2016-09-06 16:00:14 +0300

    indentations

--- example.rb
+++ example.rb
@@ -2,9 +2,9 @@
 
 require './ParallelWorker'
 
- PW = ParallelWorker.new()
- PW.debug_mode = true
- PW.set_callback(callback: lambda {|item,ext_obj| sleep 5*item ; puts "Done ,,, I am dead my Pid is #{Process.pid}"})
- PW.set_data(data: (1..10).to_a)
- PW.run()
+PW = ParallelWorker.new()
+PW.debug_mode = true
+PW.set_callback(callback: lambda {|item,ext_obj| sleep 5*item ; puts "Done ,,, I am dead my Pid is #{Process.pid}"})
+PW.set_data(data: (1..10).to_a)
+PW.run()
 
